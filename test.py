ebb9af8c14fad30623fd8424fd0a54d710823c93
zimaroni <57230091+zimaroni@users.noreply.github.com>
2019-11-01 10:18:14 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import rospy
-
-from trajectory_msgs.msg import JointTrajectory
-from trajectory_msgs.msg import JointTrajectoryPoint
-from std_msgs.msg import String
-
-# initialize the node
-rospy.init_node('arm_mover')
