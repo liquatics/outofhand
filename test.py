c80b1a8755ef262b70d6a0481f223c36571e427f
zimaroni <57230091+zimaroni@users.noreply.github.com>
2019-11-01 10:06:02 -0500

    Update test.py
    Just checking on my own to see if this works

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-# this is a test
-# test received
+#!/usr/bin/env python
+import rospy
+
+from trajectory_msgs.msg import JointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+from std_msgs.msg import String
+
+# initialize the node
+rospy.init_node('arm_mover')
