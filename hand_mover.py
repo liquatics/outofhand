8eeda351241a90a022882ff5dbb632b81860dca2
zimaroni <57230091+zimaroni@users.noreply.github.com>
2019-11-04 11:26:18 -0600

    Update hand_mover.py

--- hand_mover.py
+++ hand_mover.py
@@ -12,14 +12,35 @@ rospy.init_node('hand_mover')
 rospy.Subscriber("/hand_command", String, gesture_callback)
 
 # publish the /hand_controller/command for actions
-head_pub = rospy.Publisher('/hand_controller/command',
+hand_pub = rospy.Publisher('/hand_controller/command',
                            JointTrajectory, queue_size=10)
 
 # arrays for the different fingers on the hand
 hand_array = []
 
-# ******************************************************************
-#                       setup for Odds-Evens
-# ******************************************************************
+# *****************************************************************************
+#                               setup for Odds-Evens
+# *****************************************************************************
+
+# initialize odds and assign hand_array
 odds = JointTrajectory()
 odds.joint_names = hand_array
+# set points for odds finger positions, only has pointer finger extend
+# JointTrajectoryPoint of 0 = fully extended, 1 = fully retracted
+odds_points = JointTrajectoryPoint()
+odds_points.positions = []
+odds_points.velocities = []*
+# send points array back to odds for loop commands
+odds.points = [odds_point]
+
+
+# initialize evens and assign hand_array
+evens = JointTrajectory()
+evens.joint_names = hand_array
+# set points for evens finger positions, only has pointer/middle finger extend
+# JointTrajectoryPoint of 0 = fully extended, 1 = fully retracted
+evens_points = JointTrajectoryPoint()
+evens_points.positions = []
+evens_points.velocities = []*
+# send points array back to evens for loop commands
+evens.points = [evens_points]
