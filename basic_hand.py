2624d0315b79fad6392da523f794b3b239e8ddda
zimaroni <57230091+zimaroni@users.noreply.github.com>
2019-11-12 12:12:34 -0600

    Format and Hand Gestures
    Minor formatting changes to the code comments. Additional hand gesture additions for the pointer, two_finger (aka peace), and thumbs_up

--- basic_hand.py
+++ basic_hand.py
@@ -8,19 +8,17 @@ rospy.init_node('basic_hand')
 
 # ================ #
 # SUBSCRIBERS      #
-# ------------------------------------------ #
-# Will evaluate info sent on these channels. #
-# ========================================== #
-
+# --------------------------------------------------------------------------- #
+# Will evaluate info sent on these channels directly from evaluator terminal. #
+# =========================================================================== #
 rospy.Subscriber('/hand_utility_gestures', String, setHandGesture)
 rospy.Subscriber('/arm_utility_gestures', String, setArmGesture)
 
 # ================ #
-# Publishers      #
-# ----------------------------------------------------------------------- #
-# Will send info to these channels.                                       #
-# ======================================================================= #
-
+# PUBLISHERS      #
+# --------------------------------------------------------------------------- #
+# Will send info to these channels based on input from evaluator.             #
+# =========================================================================== #
 hand_pub = rospy.Publisher('/hand_controller/command',
                            JointTrajectory, queue_size=10)
 arm_pub = rospy.Publisher('/right_arm/request',
@@ -29,18 +27,29 @@ arm_watchdog = rospy.Publisher('/right_arm/watchdog',
                                 Empty, queue_size=10)
 
 # ================ #
-# Definitions      #
-# ----------------------------------------------------------------------- #
-# Variables used throughout the node.                                     #
-# Hand gesture parameters are defined as:                                 #
-#                                                                         #
-# [thumb, pointer, middle, ring, pinky]                                   #
-# ======================================================================= #
-
-hand_gesture = JointTrajectory()
-hand_gesture.points = [JointTrajectoryPoint(positions=open_hand)]
+# DEFINITIONS      #
+# --------------------------------------------------------------------------- #
+# Variables used throughout the node.                                         #
+# Hand gesture parameters are defined as:                                     #
+#                                                                             #
+# [thumb, pointer, middle, ring, pinky]                                       #
+# =========================================================================== #
+pointer = [1, 0, 1, 1, 1]
+peace = [1, 0, 0, 1, 1]
+thumbs_up = [0, 1, 1, 1, 1]
 fist = [1, 1, 1, 1, 1]
 open_hand = [0, 0, 0, 0, 0]
+
+hand_gesture1 = JointTrajectory()
+hand_gesture1.points = [JointTrajectoryPoint(positions=pointer)]
+hand_gesture2 = JointTrajectory()
+hand_gesture2.points = [JointTrajectoryPoint(positions=peace)]
+hand_gesture3 = JointTrajectory()
+hand_gesture3.points = [JointTrajectoryPoint(positions=thumbs_up)]
+hand_gesture4 = JointTrajectory()
+hand_gesture4.points = [JointTrajectoryPoint(positions=fist)]
+hand_gesture5 = JointTrajectory()
+hand_gesture5.points = [JointTrajectoryPoint(positions=open_hand)]
 
 arm_gesture = JointTrajectory()
 arm_names = ['shoulder_flexation_joint',
