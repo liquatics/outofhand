4083dca09bd3c44cfa2f69ef3352f5ec51721f98
zimaroni <57230091+zimaroni@users.noreply.github.com>
2019-11-25 10:24:55 -0600

    Update arm_tester.py
    Created JointTrajectory setups for all the different commands to eliminate altering the file each time we want to change position. Also tried to reimplement the callback function

--- arm_tester.py
+++ arm_tester.py
@@ -21,18 +21,13 @@ arm_pub = rospy.Publisher('/right_arm_controller/command',
 # ================ #
 # DEFINITIONS      #
 # --------------------------------------------------------------------------- #
-# Variables used throughout the node.                                         #
-# Hand gesture parameters do not need an array and are defined as:            #
-# [thumb, pointer, middle, ring, pinky]                                       #
-#                                                                             #
 # Arm gesture parameters only include shoulder_medial_joint and elbow_joint.  #
-# All others in the array should be set as 0.                                 #
+# All others in the array should be set empty.                                #
+# 2 is the max value for elbow_joint, while -2 is the min                     #
+# 3.2 indicates a 180 degree rotation for shoulder_medial_joint               #
 # =========================================================================== #
 
 arm_names = ['shoulder_medial_joint', 'elbow_joint']
-
-# 2 is max elbow_joint -2 is min
-# 3.2 is 180 degrees
 
 test = [0, -1.6]
 default = [0, 0]
@@ -41,45 +36,80 @@ max_right = [3.2, 2]
 middle = [1.6, 1]
 
 
-arm_gesture2 = JointTrajectory()
-arm_gesture2.joint_names = arm_names
+gest_test = JointTrajectory()
+gest_test.joint_names = arm_names
+gest_test_points = JointTrajectoryPoint()
+gest_test_points.positions = test
+gest_test_points.time_from_start = rospy.Duration(2)
+gest_test.points = [gest_test_points]
 
-arm_gesture_points2 = JointTrajectoryPoint()
-arm_gesture_points2.positions = max_right
-arm_gesture_points2.time_from_start = rospy.Duration(2)
-arm_gesture2.points = [arm_gesture_points2]
+gest_def = JointTrajectory()
+gest_def.joint_names = arm_names
+gest_def_points = JointTrajectoryPoint()
+gest_def_points.positions = default
+gest_def_points.time_from_start = rospy.Duration(2)
+gest_def.points = [gest_def_points]
+
+gest_maxl = JointTrajectory()
+gest_maxl.joint_names = arm_names
+gest_maxl_points = JointTrajectoryPoint()
+gest_maxl_points.positions = max_left
+gest_maxl_points.time_from_start = rospy.Duration(2)
+gest_maxl.points = [gest_maxl_points]
+
+gest_maxr = JointTrajectory()
+gest_maxr.joint_names = arm_names
+gest_maxr_points = JointTrajectoryPoint()
+gest_maxr_points.positions = max_right
+gest_maxr_points.time_from_start = rospy.Duration(2)
+gest_maxr.points = [gest_maxr_points]
+
+gest_mid = JointTrajectory()
+gest_mid.joint_names = arm_names
+gest_mid_points = JointTrajectoryPoint()
+gest_mid_points.positions = middle
+gest_mid_points.time_from_start = rospy.Duration(2)
+gest_mid.points = [gest_mid_points]
 
 
 # ================== #
 # Callback Functions #
-# ----------------------------------------------------------------------- #
-# Evaluates an action and sets the gesture points to reflect that action. #
-# ======================================================================= #
+# --------------------------------------------------------------------------- #
+# Evaluates an action and sets the gesture points to reflect that action.     #
+# =========================================================================== #
 
+def setArmGesture(msg):
+    global position
+    position.data = msg.data
 
-# def setArmGesture(msg):
-#     global arm_gesture
-#
-#     if msg == "lower":
-#         arm_gesture = arm_gesture1
-#     else:
-#         arm_gesture.points = [JointTrajectoryPoint(positions=raised)]
-#
-#
-# arm_gesture = JointTrajectory()
-# arm_gesture.joint_names = arm_names
+    if position.data == "test":
+        arm_gesture.points = gest_test.points
+    elif position.data == "default":
+        arm_gesture.points = gest_def.points
+    elif position.data == "max_left":
+        arm_gesture.points = gest_maxl.points
+    elif position.data == "max_right":
+        arm_gesture.points = gest_maxr.points
+    elif position.data == "middle":
+        arm_gesture.points = gest_mid.points
+
 
 # ================ #
 # SUBSCRIBERS      #
 # --------------------------------------------------------------------------- #
 # Will evaluate info sent on these channels directly from evaluator terminal. #
 # =========================================================================== #
-#rospy.Subscriber("/arm_gestures", String, setArmGesture)
 
-empty_gesture = Empty()
+rospy.Subscriber("/arm_gestures", String, setArmGesture)
+
+
+# ================ #
+# SETUP            #
+# --------------------------------------------------------------------------- #
+# Helps establish setup for the main loop.                                    #
+# =========================================================================== #
 
 rate = rospy.Rate(10)
-gesture_state = "IDLE"
 
 # Fruit loop
 # while not rospy.is_shutdown():
